db451119814947d5f1fe48f23723b028f3a74350
Peter Molnar <petermolnar.hu@gmail.com>
2022-04-10 18:02:22 +0200

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Peter Molnar on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
