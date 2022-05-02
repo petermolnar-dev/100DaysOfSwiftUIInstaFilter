2644d40345c0c6d06029170fc2d81805e015f5ac
Peter Molnar <petermolnar.hu@gmail.com>
2022-05-02 19:35:29 +0200

    Adding the image processing.

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -17,6 +17,9 @@ struct ContentView: View {
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -38,6 +41,9 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in
+                            applyProcessing()
+                        }
                 }
                 .padding(.vertical)
                 
@@ -68,11 +74,24 @@ struct ContentView: View {
         guard let inputImage = inputImage else {
             return
         }
-        image = Image(uiImage: inputImage)
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
     func save() {
         
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
